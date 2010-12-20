7c2f4b21829bd597fb0273d4b39969ce79b50f2d
Fletcher T. Penney <fletcher@fletcherpenney.net>
2010-12-19 20:59:57 -0500

    add --batch option to process multiple files at once; exposes memory leaks

--- markdown.c
+++ markdown.c
@@ -51,16 +51,14 @@ void version(const char *progname)
 }
 
 int main(int argc, char * argv[]) {
-	g_type_init();
     int numargs;            /* number of filename arguments */
     int i;
 
     GString *inputbuf;
     char *out;              /* string containing processed output */
 
-	PATHINFO *filename;
-	GFile *file;
-
+	GString *file;
+	char *fake;
     FILE *input;
     FILE *output;
     char curchar;
@@ -164,15 +162,6 @@ int main(int argc, char * argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    /* we allow "-" as a synonym for stdout here */
-    if (opt_output == NULL || strcmp(opt_output, "-") == 0)
-        output = stdout;
-    else if (!(output = fopen(opt_output, "w"))) {
-        perror(opt_output);
-        return 1;
-    }
-
-    inputbuf = g_string_new("");   /* string for concatenated input */
 
     numargs = argc - 1;
 
@@ -181,6 +170,7 @@ int main(int argc, char * argv[]) {
 			appropriate extension */
 		
 	       for (i = 0; i < numargs; i++) {
+		    	inputbuf = g_string_new("");   /* string for concatenated input */
 				/* Read file */
 	            if ((input = fopen(argv[i+1], "r")) == NULL) {
 	                perror(argv[i+1]);
@@ -190,27 +180,41 @@ int main(int argc, char * argv[]) {
 	                g_string_append_c(inputbuf, curchar);
 	            fclose(input);
 
-				filename = SplitPath(argv[i+1]);
-				
-				fprintf(stdout, "%s\n%s\n%s\n", filename->directory, filename->filename, filename->extension);
-				
-				GFile *file = g_file_new_for_path(argv[i+1]);
-				fprintf(stdout, "%s\n%s\n", g_file_get_path(file), g_file_get_basename(file));
-			
-				return 1;
+				/* remove file extension, if present */
+				fake = argv[i+1];
+				if (strrchr(fake, '.') != NULL) {
+					int count = strrchr(fake,'.') - fake;
+					if (count != 0) {
+						fake[count] = '\0';
+					}
+				}
+
+				file = g_string_new(fake);
+				if (output_format == HTML_FORMAT) {
+					g_string_append(file,".html");
+				} else {
+					g_string_append(file,".tex");
+				}
+
 				/* open output file */
-				if (!(output = fopen(filename->filename, "w"))) {
+				if (!(output = fopen(file->str, "w"))) {
 			        perror(opt_output);
 			        return 1;
 			    }
 				
 			    out = markdown_to_string(inputbuf->str, extensions, output_format);
+
 			    fprintf(output, "%s\n", out);
+				fclose(output);
+				g_string_free(file,false);
 			    free(out);
+			    g_string_free(inputbuf, true);
 	       }
 		
 	} else {
 	    /* Read input from stdin or input files into inputbuf */
+
+	    inputbuf = g_string_new("");   /* string for concatenated input */
 
 	    if (numargs == 0) {        /* use stdin if no files specified */
 	        while ((curchar = fgetc(stdin)) != EOF)
@@ -228,6 +232,15 @@ int main(int argc, char * argv[]) {
 	            fclose(input);
 	       }
 	    }
+
+	    /* we allow "-" as a synonym for stdout here */
+	    if (opt_output == NULL || strcmp(opt_output, "-") == 0)
+	        output = stdout;
+	    else if (!(output = fopen(opt_output, "w"))) {
+	        perror(opt_output);
+	        return 1;
+	    }
+
 
 	    out = markdown_to_string(inputbuf->str, extensions, output_format);
 	    fprintf(output, "%s\n", out);
