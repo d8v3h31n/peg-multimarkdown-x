05d105eefcca8599685be421cff715874b2053d7
Fletcher T. Penney <fletcher@fletcherpenney.net>
2011-02-18 20:20:23 -0500

    start to use NSMutableString instead of GString

--- glib2Cocoa.sh
+++ glib2Cocoa.sh
@@ -22,4 +22,18 @@ perl -pi -e 's/%d\\t/%td\\t/g' peg-0.1.4/tree.c
 # typecast strlen
 perl -pi -e 's/\+ \(strlen\(yy/+ (int)(strlen(yy/g' markdown_parser.leg
 
+
+# Change to imports
+perl -pi -e 's/#include "utility_functions.c"/#import "utility_functions.c"/g' markdown_parser.leg
+perl -pi -e 's/#include "parsing_functions.c"/#import "parsing_functions.c"/g' markdown_parser.leg
+
+
+
 # convert code from glib2 based g_strings to Cocoa NSMutableString
+perl -pi -e 's/GString/NSMutableString/g' *.c *.h *.leg
+
+perl -pi -e 's/g_string_append_printf\((.*?),\s*(".*?")\s*\)/[$1 appendString:\@$2]/g' *.c *.leg
+
+perl -pi -e 's/g_string_append_printf\((.*?),\s*(.*)\);/[$1 appendFormat:\@$2];/g' *.c *.leg
+
+perl -pi -e 's/char \*/NSString */g' *.c *.h *.leg
