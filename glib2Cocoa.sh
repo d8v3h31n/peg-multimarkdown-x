fb43808df1e94cd4c4ccf34610105cfaef64010f
Fletcher T. Penney <fletcher@fletcherpenney.net>
2011-02-19 19:29:25 -0500

    update cocoa script for *.m

--- glib2Cocoa.sh
+++ glib2Cocoa.sh
@@ -2,16 +2,16 @@
 
 
 # Change STR to STRING
-perl -pi -e 's/\bSTR\b/STRING/g' *.c *.h
+perl -pi -e 's/\bSTR\b/STRING/g' *.m *.h
 
 
 # Change link to Link
-perl -pi -e 's/(\s)link\b/$1Link/g' *.c *.leg *.h
-perl -pi -e 's/\(link\)/(Link)/g' *.c *.leg *.h
+perl -pi -e 's/(\s)link\b/$1Link/g' *.m *.leg *.h
+perl -pi -e 's/\(link\)/(Link)/g' *.m *.leg *.h
 
 # Change output of print_tree for compatibility
-perl -pi -e 's/0x%x:/%p:/g' markdown_lib.c
-perl -pi -e 's/\(int\)elt/elt/g' markdown_lib.c	
+perl -pi -e 's/0x%x:/%p:/g' markdown_lib.m
+perl -pi -e 's/\(int\)elt/elt/g' markdown_lib.m	
 
 
 # Tweak peg
@@ -24,16 +24,16 @@ perl -pi -e 's/\+ \(strlen\(yy/+ (int)(strlen(yy/g' markdown_parser.leg
 
 
 # Change to imports
-perl -pi -e 's/#include "utility_functions.c"/#import "utility_functions.c"/g' markdown_parser.leg
-perl -pi -e 's/#include "parsing_functions.c"/#import "parsing_functions.c"/g' markdown_parser.leg
+perl -pi -e 's/#include "utility_functions.m"/#import "utility_functions.m"/g' markdown_parser.leg
+perl -pi -e 's/#include "parsing_functions.m"/#import "parsing_functions.m"/g' markdown_parser.leg
 
 
 
 # convert code from glib2 based g_strings to Cocoa NSMutableString
-perl -pi -e 's/GString/NSMutableString/g' *.c *.h *.leg
+perl -pi -e 's/GString/NSMutableString/g' *.m *.h *.leg
 
-perl -pi -e 's/g_string_append_printf\((.*?),\s*(".*?")\s*\)/[$1 appendString:\@$2]/g' *.c *.leg
+perl -pi -e 's/g_string_append_printf\((.*?),\s*(".*?")\s*\)/[$1 appendString:\@$2]/g' *.m *.leg
 
-perl -pi -e 's/g_string_append_printf\((.*?),\s*(.*)\);/[$1 appendFormat:\@$2];/g' *.c *.leg
+perl -pi -e 's/g_string_append_printf\((.*?),\s*(.*)\);/[$1 appendFormat:\@$2];/g' *.m *.leg
 
-perl -pi -e 's/char \*/NSString */g' *.c *.h *.leg
+perl -pi -e 's/char \*/NSString */g' *.m *.h *.leg
