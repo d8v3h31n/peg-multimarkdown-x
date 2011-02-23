91cf819fde2dea5e1476d5db3e06facf424b164a
Fletcher T. Penney <fletcher@fletcherpenney.net>
2011-02-23 11:37:33 -0500

    cleanup whitespace

--- markdown.c
+++ markdown.c
@@ -187,7 +187,7 @@ int main(int argc, char * argv[]) {
                 /* Display metadata on request */
                 if (opt_extract_meta) {
                     out = extract_metadata_value(inputbuf->str, extensions, opt_extract_meta);
-            		if (out != NULL) fprintf(stdout, "%s\n", out);
+                    if (out != NULL) fprintf(stdout, "%s\n", out);
                     return(EXIT_SUCCESS);
                 }
                 
@@ -205,7 +205,7 @@ int main(int argc, char * argv[]) {
                     g_string_append(file,".html");
                 } else if (output_format == OPML_FORMAT) {
                     g_string_append(file,".opml");
-	            } else {
+                } else {
                     g_string_append(file,".tex");
                 }
 
--- markdown_lib.c
+++ markdown_lib.c
@@ -153,16 +153,16 @@ GString * markdown_to_g_string(char *text, int extensions, int output_format) {
 
     formatted_text = preformat_text(text);
 
-	if (output_format == OPML_FORMAT) {
-		result = parse_markdown_for_opml(formatted_text->str, extensions);
-	} else {
-	    references = parse_references(formatted_text->str, extensions);
-	    notes = parse_notes(formatted_text->str, extensions, references);
-	    labels = parse_labels(formatted_text->str, extensions, references, notes);
-	    result = parse_markdown_with_metadata(formatted_text->str, extensions, references, notes, labels);
+    if (output_format == OPML_FORMAT) {
+        result = parse_markdown_for_opml(formatted_text->str, extensions);
+    } else {
+        references = parse_references(formatted_text->str, extensions);
+        notes = parse_notes(formatted_text->str, extensions, references);
+        labels = parse_labels(formatted_text->str, extensions, references, notes);
+        result = parse_markdown_with_metadata(formatted_text->str, extensions, references, notes, labels);
 
-	    result = process_raw_blocks(result, extensions, references, notes, labels);
-	}
+        result = process_raw_blocks(result, extensions, references, notes, labels);
+    }
 
     g_string_free(formatted_text, TRUE);
 
@@ -170,10 +170,10 @@ GString * markdown_to_g_string(char *text, int extensions, int output_format) {
 
     free_element_list(result);
 
-	if (output_format != OPML_FORMAT) {
-		free_element_list(references);
-	    free_element_list(labels);
-	}
+    if (output_format != OPML_FORMAT) {
+        free_element_list(references);
+        free_element_list(labels);
+    }
     return out;
 }
 
