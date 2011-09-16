ff267490a934afd70ff731e9be94c1889408f124
Fletcher T. Penney <fletcher@fletcherpenney.net>
2011-09-16 09:53:42 -0400

    update manual

--- makewin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-make clean
-make
-rm *.o
-
-/usr/bin/i586-mingw32msvc-cc -c -Wall -O3 -ansi markdown*.c GLibFacade.c
-/usr/bin/i586-mingw32msvc-cc markdown*.o GLibFacade.o -Wl,--dy,--warn-unresolved-symbols,-lglib-2.0 -o multimarkdown.exe
